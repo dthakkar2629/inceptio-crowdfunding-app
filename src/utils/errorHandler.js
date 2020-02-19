db30a2bf55b37c22cfb1d24145dbb627c851070d
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2020-02-19 16:10:14 +0530

    Network error feedback msg

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,7 @@
 const errorMessage = (error) => {
   console.log({error});
-  if (error.response) {console.log(error.response.data.msg); return error.response.data.msg;}
+  if(error.message === "Network Error") return "No internet connection!";
+  else if (error.response) {console.log(error.response.data.msg); return error.response.data.msg;}
   else if (error.request) {console.log(error.request.statusText); return error.request.statusText;}
   else {console.log(error.message); return error.message;}
 };
